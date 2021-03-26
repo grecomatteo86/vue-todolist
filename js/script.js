46e80681cab034c6c367c94b9f68fd30c03078d9
Matteo <grecomatteo86@gmail.com>
2021-03-26 16:26:06 +0100

    v-for setting

--- js/script.js
+++ js/script.js
@@ -2,7 +2,12 @@ var app = new Vue (
   {
     el:'#app',
     data:{
-      
+      comix:[
+        'Possessione Diabolica',
+        'L\'alba dei morti viventi',
+        'Canale 666',
+        'Le notti della luna piena'
+      ]
     }
   }
 );
