e237bcc9f25dd306c05bd28793201dccb33e1dad
Matteo <grecomatteo86@gmail.com>
2021-03-26 17:27:26 +0100

    addTitle function

--- js/script.js
+++ js/script.js
@@ -2,16 +2,21 @@ var app = new Vue (
   {
     el:'#app',
     data:{
+      userTitle:'',
       comix:[
         'Possessione Diabolica',
         'L\'alba dei morti viventi',
         'Canale 666',
         'Le notti della luna piena'
-      ]
+      ],
+
     },
     methods:{
       deleteItem: function(index){
         this.comix.splice(index,1);
+      },
+      addTitle: function(){
+        this.comix.push(this.userTitle);
       }
     }
   }
